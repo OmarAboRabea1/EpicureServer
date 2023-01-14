eed999c51d2f0480905cb65b58dd409be37e6670
Omar Abo Rabea <aborabeeomar@gmail.com>
2023-01-14 23:36:45 +0200

    add s

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,4 +5,5 @@ const connectDb = async () => {
   await mongoose.connect("mongodb+srv://Omar_Abo_Rabea:OmarAboRabea7@epicuredb.vekk4eg.mongodb.net/?retryWrites=true&w=majority");
 };
 
+
 export { connectDb };
